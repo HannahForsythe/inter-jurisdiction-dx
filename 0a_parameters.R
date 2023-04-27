303af241c64aa1f6967e38c145bdbee94c689b80
Forsythe <ForsytheH@michigan.gov>
2023-04-27 16:28:26 -0400

    initial commit

---
+++ 0a_parameters.R
@@ -0,0 +1,8 @@
+# What is the state code for your home jurisdiction?
+home_st <- 'MI'
+# What are the state codes of the jurisdictions you exchange data with?
+partner_st <- c('WI')
+# When did data exchange begin (earliest date)?
+exchg_start <- as.Date('2019-01-01')
+# What are the fips codes of border counties in your state?
+border_fips <- c(26041, 26043, 26053, 26071, 26109, 26131)
