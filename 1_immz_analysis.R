edb463c1874cbbc0714cf5469125772ba1b419fb
Forsythe <forsytheh@michigan.gov>
2024-01-05 11:32:45 -0500

    adding in stratification of shared immunizations by category and type of antigen(s); requires adding RColorBrewer to the libraries

--- 1_immz_analysis.R
+++ 1_immz_analysis.R
@@ -13,6 +13,11 @@ demog <- read_delim("input/demog.zip") %>%
 
 immz_shared <- immz %>%
   filter(reporting_iis %in% c("P", "P1", "P2"))
+
+crosswalk_cvx <- read_csv(
+  "input/crosswalk_CVX_nm.csv"
+) %>%
+  clean_names()
 #-----------------------------------------------------------------------------#
 # 1. Number of records added to the native IIS, per year
 #-----------------------------------------------------------------------------#
@@ -84,7 +89,65 @@ iz_cnty_map <- plot_usmap(data=iz_cnty_2, values="n_shots", include=home_st) +
 iz_cnty_map
 
 #-----------------------------------------------------------------------------#
-# 3. Average reporting delay
+# 3. stratify by category and antigen group
+#-----------------------------------------------------------------------------#
+# this is the raw data, by cvx
+iz_cvx <- immz_shared %>%
+  left_join(crosswalk_cvx, by = "cvx") %>%
+  group_by(vax_cat, vax_cat_antigen, cvx) %>%
+  summarise(n = n_distinct(vax_event_id)) 
+
+# create a table by broad category (seasonal, child, adol, adu, other)
+iz_cat <- iz_cvx %>%
+  group_by(vax_cat) %>%
+  summarise(n = sum(n)) %>%
+  arrange(-n)
+
+iz_cat_tbl <- iz_cat %>%
+  adorn_totals() %>%
+  rename(`category` = vax_cat) %>%
+  # head() %>%
+  flextable() %>%
+  autofit() %>%
+  set_caption(
+    paste0("Out-of-state immunizations reported to ", home_st)
+  )
+iz_cat_tbl
+
+# create a graph crossing broad vaccine categories with antigen type
+iz_type <- iz_cvx %>%
+  group_by(vax_cat, vax_cat_antigen) %>%
+  summarise(n = sum(n)) %>%
+  ungroup() %>%
+  arrange(vax_cat, -n) %>%
+  mutate(
+    order = row_number()
+  )
+  
+# create a palette with 17 colors
+Dark2_17 <- c("#1B9E77", "#6E8243", "#D55E00", "#B96439", "#8D6B86", "#8A62AB",
+              "#BC4399", "#DE3083", "#A66753", "#6E9E24", "#CEAA07", "#D59D08", 
+              "#666666", "#96A713", "darkgreen", "#9D7426", "#0072B2")
+
+iz_type_fig <- iz_type %>%
+  ggplot(
+    aes(y = vax_cat, 
+        x = n, 
+        fill = reorder(vax_cat_antigen, -order))
+  ) +
+  geom_col(color = "black") +
+  labs(
+    title = "Vaccinations received out of state, by category",
+    x = "",
+    y = ""
+  ) +
+  # scale_fill_discrete(name = "Vaccine antigen(s)") +
+  theme_bw() +
+  scale_fill_manual(values = Dark2_17, name = "Vaccine antigen(s)")
+iz_type_fig
+
+#-----------------------------------------------------------------------------#
+# 4. Average reporting delay
 #-----------------------------------------------------------------------------#
 rpt_delay <- immz %>%
   filter(Sys.Date() > admin_date) %>%
@@ -128,8 +191,9 @@ rpt_delay_hist_P <- rpt_delay %>%
   theme(legend.position="bottom")
 rpt_delay_hist_P
 
+
 #-----------------------------------------------------------------------------#
-# 4. export results
+# 5. export results
 #-----------------------------------------------------------------------------#
 # Create the output (sub)directory
 if (!dir.exists("output/1_immz")) {
@@ -143,6 +207,9 @@ write.csv(iz_yr,
 write.csv(iz_cnty, 
           file = paste0("output/1_immz/iz_cnty-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".csv"),
           row.names = FALSE)
+write.csv(iz_cvx, 
+          file = paste0("output/1_immz/iz_cvx_cat_antigen-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".csv"),
+          row.names = FALSE)
 write.csv(rpt_delay, 
           file = paste0("output/1_immz/rpt_delay-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".csv"),
           row.names = FALSE)
@@ -152,11 +219,16 @@ save_as_docx(iz_yr_tbl,
              path = paste0("output/1_immz/iz_yr-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".docx"))
 save_as_docx(iz_cnty_tbl,
              path = paste0("output/1_immz/iz_cnty-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".docx"))
+save_as_docx(iz_cat_tbl,
+             path = paste0("output/1_immz/iz_cat-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".docx"))
 
 # export figures as png files
+ggsave(filename = paste0("output/1_immz/iz_cnty_map-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".png"),
+       plot = iz_cnty_map)
+ggsave(filename = paste0("output/1_immz/iz_type_fig-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".png"),
+       plot = iz_type_fig)
 ggsave(filename = paste0("output/1_immz/rpt_delay-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".png"),
        plot = rpt_delay_hist_N)
 ggsave(filename = paste0("output/1_immz/rpt_delay-", paste0(partner_st, collapse = '-'), "-", format(Sys.Date(), "%Y%m%d"),".png"),
        plot = rpt_delay_hist_P)
-ggsave(filename = paste0("output/1_immz/iz_cnty_map-", home_st, "-", format(Sys.Date(), "%Y%m%d"),".png"),
-       plot = iz_cnty_map)
+
